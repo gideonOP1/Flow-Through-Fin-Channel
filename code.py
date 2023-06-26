a6a31bbaf800b582ada72d9b0464b30b261babd3
gideonOP1 <111236658+gideonOP1@users.noreply.github.com>
2023-06-26 21:09:23 +0530

    Update code.py

--- code.py
+++ code.py
@@ -4,93 +4,94 @@ Created on Wed Jun 14 23:56:26 2023
 
 @author: Gideon
 """
-
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
-plt.rcParams['figure.dpi']="500" 
+
+plt.rcParams['figure.dpi']="100" 
 
 grid_point=100
 domain_size=1
-n_iterations=500
-kinematic_viscoscity=0.1
-density=2.0
+n_iterations=100
+kinematic_viscoscity=0.5
+density=1
 vel=10
 
-
+ 
 
 pressure_poisson_iterations=100
+#@jit (nopython=True)
+
+element_length=domain_size/(grid_point-1)
+x=np.linspace(0.0,domain_size,grid_point)
+y=np.linspace(0.0,domain_size,grid_point)
+
+X,Y=np.meshgrid(x,y)
+
+################## Time Step Limiting ##############
+time_step = min(0.25*(element_length**2)/kinematic_viscoscity,
+              4.0*kinematic_viscoscity/(vel**2))
+
+## Initial Conditions ##
+
+
+
+####################### Central diff method discretized for Square Mesh ###########################
+
+def central_diff_x(f):   
+    diff=np.zeros_like(f)
+    diff[1:-1,1:-1]=(
+        
+       f[1:-1 , 2: ] - f[1:-1, 0:-2]
+        
+    )/(
+        2*element_length
+    )
+    return diff
+
+def central_diff_y(f):   
+    diff=np.zeros_like(f)
+    diff[1:-1, 1:-1]=(
+    
+        f[2: , 1:-1] - f[0:-2 , 1:-1]
+    
+    )/(
+        2*element_length
+    )
+    return diff
+
+
+
+######################### Laplace operator discretized for Square Mesh #############################
+ 
+def laplace(f):    
+    diff=np.zeros_like(f)
+    diff[1:-1,1:-1]=(
+        f[1:-1, 0:-2]
+        +
+        f[0:-2,1:-1]
+        -
+        4
+        *
+        f[1:-1,1:-1]
+        +
+        f[1:-1,2: ]
+        +
+        f[2: ,1:-1]
+    )/(
+    element_length**2
+    )
+    return diff
+
+
+
 
 def main():
-    element_length=domain_size/(grid_point-1)
-    x=np.linspace(0.0,domain_size,grid_point)
-    y=np.linspace(0.0,domain_size,grid_point)
-    
-    X,Y=np.meshgrid(x,y)
-    
-    ################## Time Step Limiting ##############
-    
-    
-    time_step = min(0.25*element_length*element_length/kinematic_viscoscity,
-                  4.0*kinematic_viscoscity/vel/vel)
-  
-    ## Initial Conditions ##
-    
+       
+        
     u_prev=np.zeros_like(X)
     v_prev=np.zeros_like(X)
-    p_prev=np.zeros_like(X) 
-
-
-    ## Descritised Differential Operators ##
-
-    ####################### Central diff method discretized for Square Mesh ###########################
-   
-    def central_diff_x(f):   
-        diff=np.zeros_like(f)
-        diff[1:-1,1:-1]=(
-            
-           f[1:-1 , 2: ] - f[1:-1, 0:-2]
-            
-        )/(
-            2*element_length
-        )
-        return diff
-
-    def central_diff_y(f):   
-        diff=np.zeros_like(f)
-        diff[1:-1, 1:-1]=(
-        
-            f[2: , 1:-1] - f[0:-2 , 1:-1]
-        
-        )/(
-            2*element_length
-        )
-        return diff
-    
-    ######################### Laplace operator discretized for Square Mesh #############################
-        
-    def laplace(f):    
-        diff=np.zeros_like(f)
-        diff[1:-1,1:-1]=(
-            f[1:-1, 0:-2]
-            +
-            f[0:-2,1:-1]
-            -
-            4
-            *
-            f[1:-1,1:-1]
-            +
-            f[1:-1,2: ]
-            +
-            f[2: ,1:-1]
-        )/(
-        element_length**2
-        )
-        return diff
-    
-    
-        
-    
+    p_prev=np.zeros_like(X)    
     
     for _ in tqdm(range(n_iterations)):
         
@@ -106,9 +107,10 @@ def main():
         
         
     
-        u_prev[48:52,0]=vel
-        u_prev[48:52, -1]=vel
+        u_prev[40:60, 0]= vel
+        u_prev[48:52,-1]= vel
         
+        ######################### Solving for velocity ignoring the Pressure Gradient #############################      
        
         u_int=(
             
@@ -117,6 +119,7 @@ def main():
                 time_step*(
                   -  
                   (
+                     
                        u_prev*central_diff_x(u_prev)
                        +
                        v_prev*central_diff_y(u_prev)
@@ -177,13 +180,9 @@ def main():
             p_next[0,:]=p_next[1,:]
             p_next[:,0]=p_next[:,1]
             p_next[-1,:]=p_next[0,:]
-          \
             
-           
-        
             p_prev=p_next
-            
-        
+                    
         ######################### Velocity Correction Step #############################
         
         u_next=(
@@ -202,9 +201,8 @@ def main():
             central_diff_y(p_next)
         )
         
-        
         ######################### Channel Boundary Conditions #############################
-       
+      
         u_next[8:12,20:80]=0
         v_next[8:12,20:80]=0
         
@@ -232,25 +230,14 @@ def main():
         u_next[88:92,20:80]=0
         v_next[88:92,20:80]=0
         
-        
-        
-        
-        
-        
-        
-        
-        
-       
-        
+
         ######################### Advance In Time #############################
         
         u_prev=u_next
         v_prev=v_next
         p_prev=p_next
             
-    
-      
-   
+
     plt.figure()
      
     plt.contourf(X,Y,p_next)
@@ -260,15 +247,15 @@ def main():
     
     ######################### Channel Boundary Display #############################
     
-    plt.axhspan(0.08,0.11,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
-    plt.axhspan(0.18,0.21,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
-    plt.axhspan(0.28,0.31,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
-    plt.axhspan(0.38,0.41,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
-    plt.axhspan(0.48,0.51,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
-    plt.axhspan(0.58,0.61,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
-    plt.axhspan(0.68,0.71,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
-    plt.axhspan(0.78,0.81,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
-    plt.axhspan(0.88,0.91,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
+    plt.axhspan(0.08,0.12,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
+    plt.axhspan(0.18,0.22,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
+    plt.axhspan(0.28,0.32,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
+    plt.axhspan(0.38,0.42,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
+    plt.axhspan(0.48,0.52,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
+    plt.axhspan(0.58,0.62,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
+    plt.axhspan(0.68,0.72,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
+    plt.axhspan(0.78,0.82,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
+    plt.axhspan(0.88,0.92,0.25,0.8 , color='black', alpha=0.75, lw=0.1)
     
     
     plt.show()
